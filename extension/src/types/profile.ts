655f7664132c49417292341cdf19d57d8de6dc9d
lakshmiuipath11-sudo <lakshmiuipath11@gmail.com>
2026-07-27 13:56:48 +0530

    Create profile.ts

---
+++ extension/src/types/profile.ts
@@ -0,0 +1,15 @@
+export interface CandidateProfile {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+
+  currentCompany: string;
+  designation: string;
+
+  experience: string;
+  linkedin: string;
+
+  skills: string[];
+  resume: string;
+}
